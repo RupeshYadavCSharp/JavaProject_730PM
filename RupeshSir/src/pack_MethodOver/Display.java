470c6d09c14286cead8f1e9376d15611007239c2
RupeshYadavCSharp <81669954+RupeshYadavCSharp@users.noreply.github.com>
2023-11-23 08:06:51 +0530

    commited test on 22 nov

---
+++ RupeshSir/src/pack_MethodOver/Display.java
@@ -0,0 +1,20 @@
+package pack_MethodOver;
+
+public class Display
+{
+    public void show(int a)
+    {
+        System.out.println("show1 called");
+    }
+    public void show(int a,String s)
+    {
+        System.out.println("show2 called ");
+    }
+
+    public void show(String s,int a)
+    {
+        System.out.println("show3 called ");
+    }
+
+
+}
---
+++ RupeshSir/src/pack_MethodOver/MainClass.java
@@ -0,0 +1,14 @@
+package pack_MethodOver;
+
+public class MainClass
+{
+    public static void main(String[] args) {
+        Display display = new Display();
+
+
+
+
+
+
+    }
+}
